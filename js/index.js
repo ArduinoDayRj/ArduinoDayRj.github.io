fa3ff53ad76d1ca90147d8f20250d0378679e1fc
lucassoares <lsoares267@gmail.com>
2018-04-19 20:09:25 -0300

    Implementando interface

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$(window).resize(function(){
+			AjustesTela();
+	});
+
+	AjustesTela();
+
+
+	function AjustesTela(){
+		if($(window).width() <= 400){
+			$('.teste').css('display', 'block');
+			$('#naoFiqueDeFora').css('font-size', '30px');
+			$('.imgRedonda').css('width', '180px');
+		}else{
+			$('.teste').css('display', 'inline-block');
+			$('#naoFiqueDeFora').css('font-size', '70px');
+			$('.imgRedonda').css('width', '100%');
+		}
+	}
+});
