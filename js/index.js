2e926bbc073f8b654e3d7cbce4b65a56a37b7d67
lucassoares <lsoares267@gmail.com>
2018-04-20 20:54:27 -0300

    :v:

--- js/index.js
+++ js/index.js
@@ -12,10 +12,12 @@ $(document).ready(function(){
 			$('.teste').css('display', 'block');
 			$('#naoFiqueDeFora').css('font-size', '30px');
 			$('.imgRedonda').css('width', '180px');
+			$('.barrinha').css('margin-top', '0px');
 		}else{
 			$('.teste').css('display', 'inline-block');
 			$('#naoFiqueDeFora').css('font-size', '70px');
 			$('.imgRedonda').css('width', '100%');
+			$('.barrinha').css('margin-top', '50px');
 		}
 	}
 });
