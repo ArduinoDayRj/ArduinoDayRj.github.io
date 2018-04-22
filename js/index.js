ae529b0fa4158a8d1457c88238c5c3ed0970ddea
lucassoares <lsoares267@gmail.com>
2018-04-22 20:44:55 -0300

    Corrigindo bugs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+	
+
 	AjustesTela();
+
+	$('#btnDireita').click(function(e){
+		$('#myCarousel2').carousel('next');
+	});
+
+	$('#btnEsquerda').click(function(e){
+		$('#myCarousel2').carousel('prev');
+	});
+	
 	
 
 	$(window).resize(function(){
@@ -13,11 +24,19 @@ $(document).ready(function(){
 			$('#naoFiqueDeFora').css('font-size', '30px');
 			$('.imgRedonda').css('width', '180px');
 			$('.barrinha').css('margin-top', '0px');
+			$('.imagemPalestrante').css('z-index', '0');
+			$('.imagemPalestrante').css('position', 'relative');
+			$('.descricaoPalestrante').css('padding-left', '25px');
+			$('#barrinhasColoridas').css('margin-top', '20px');
 		}else{
 			$('.teste').css('display', 'inline-block');
 			$('#naoFiqueDeFora').css('font-size', '70px');
 			$('.imgRedonda').css('width', '100%');
 			$('.barrinha').css('margin-top', '50px');
+			$('.imagemPalestrante').css('z-index', '2');
+			$('.imagemPalestrante').css('position', 'absolute');
+			$('.descricaoPalestrante').css('padding-left', '200px');
+			$('#barrinhasColoridas').css('margin-top', '100px');
 		}
 	}
 });
