ce5ee6b40c7535862dd6ac26e97be9cd3ea80d8f
Lucas Soares <lsoares267@gmail.com>
2018-04-23 15:24:20 -0300

    :v:

--- js/index.js
+++ js/index.js
@@ -28,6 +28,11 @@ $(document).ready(function(){
 			$('.imagemPalestrante').css('position', 'relative');
 			$('.descricaoPalestrante').css('padding-left', '25px');
 			$('#barrinhasColoridas').css('margin-top', '20px');
+			$('#footer').css('text-align', 'center');
+			$('#logos').css('float', 'none');
+			$('.logosRodape').css('float', 'none');
+			$('#imagensRodape').css('margin-top', '40px');
+			$('#imagensRodape').css('text-align', 'center');
 		}else{
 			$('.teste').css('display', 'inline-block');
 			$('#naoFiqueDeFora').css('font-size', '70px');
@@ -37,6 +42,11 @@ $(document).ready(function(){
 			$('.imagemPalestrante').css('position', 'absolute');
 			$('.descricaoPalestrante').css('padding-left', '200px');
 			$('#barrinhasColoridas').css('margin-top', '100px');
+			$('#footer').css('text-align', 'left');
+			$('.logosRodape').css('float', 'left');
+			$('#logos').css('float', 'right');
+			$('#imagensRodape').css('margin-top', '0px');
+			$('#imagensRodape').css('text-align', 'right');
 		}
 	}
 });
